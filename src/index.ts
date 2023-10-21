429a43a31a5e88a0807d0814c9312043f5a04a72
Liam Wilton-Jones <liamwiltonjones@gmail.com>
2023-10-21 12:15:09 +0100

    breaking pipeline?

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Elysia } from "elysia";
 const port = 8080;
 
 export const app = new Elysia()
-    .get("/", () => "Hello World")
+    .get("/", () => "Hello Worl")
     .listen(port);
 
 console.log(
